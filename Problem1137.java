c2fb92ee332370a8fb69ed1216401177bcf61cc0
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2020-12-14 13:54:34 -0500

    1137. N-th Tribonacci Number

--- Problem1137.java
+++ Problem1137.java
@@ -7,11 +7,11 @@ package leetCode;
 public class Problem1137 {
 	public static void main(String[] args) {
 		
-		System.out.println(climbStairs(6));
+		System.out.println(tribonacci(6));
 		
 	}
 	
-    public static int climbStairs(int n) {
+    public static int tribonacci(int n) {
     	if(n == 0) {
     		return 0;
     	} else if(n == 1) {
