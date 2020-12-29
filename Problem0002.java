165a1bfe4f7aab338065f8c5b62bd130a0ff155e
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2020-12-29 09:20:29 -0500

    2. Add Two Numbers

--- Problem0002.java
+++ Problem0002.java
@@ -6,12 +6,8 @@
  * 		int val;
  * 		ListNode next;
  * 
- * 		ListNode() {
- * 		}
- * 
- * 		ListNode(int val) {
- * 			this.val = val;
- * 		}
+ * 		ListNode() {} 
+ * 		ListNode(int val) {this.val = val;}
  * 
  *		ListNode(int val, ListNode next) {
  *			this.val = val;
