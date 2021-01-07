b256fa21989cf632ba709d4e84bea39a2be20200
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2021-01-07 18:09:20 -0500

    111. Minimum Depth of Binary Tree

--- Problem0111.java
+++ Problem0111.java
@@ -1,13 +1,46 @@
 /* 111. Minimum Depth of Binary Tree
  * https://leetcode.com/problems/minimum-depth-of-binary-tree/
- * 
- * this is scoring low, 55th and 66th percentiles.
- * there's no need to calculate beyond the length of the shortes branch + 1.
- * this currently calcultes the entire lengths of branches
  */
+
 package leetCode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Problem0111 {
+	
+    public static int minDepth(TreeNode root) {
+    	if(root == null) { return 0; }
+    	
+    	Queue<TreeNode> q = new LinkedList<TreeNode>();
+    	q.offer(root);
+    	
+    	int level = 2;
+    	while(!q.isEmpty()) {
+    		int mark = q.size();
+    		for(int i = 0; i < mark; i++) {
+    			if(q.peek().left == null && q.peek().right == null) {
+    				return level;
+    			}
+    			if(q.peek().left != null) { q.offer(q.peek().left);}
+    			if(q.peek().right != null) { q.offer(q.peek().right);}
+    			q.poll();
+    		}
+    		level++;
+    	}
+    	return 0;
+    	
+    	/* works, but slow, 55th and 66th percentiles. rework with BFS
+        if(root == null) {return 0;}
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if(left == 0 || right == 0) {
+        	return Math.max(left, right) + 1;
+        }
+        return Math.min(left, right) + 1;
+        */
+    }
+	
 	public static void main(String[] args) {
 		
 		TreeNode t1 = new TreeNode(-9);
@@ -30,16 +63,4 @@ public class Problem0111 {
 		System.out.println(minDepth(t1));
 		
 	}
-	
-    public static int minDepth(TreeNode root) {
-        if(root == null) {return 0;}
-        int left = minDepth(root.left);
-        int right = minDepth(root.right);
-        if(left == 0 || right == 0) {
-        	return Math.max(left, right) + 1;
-        }
-        return Math.min(left, right) + 1;
-    }
-	
-    
 }
