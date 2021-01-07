6344881fa9f490f813de56cf01e233eb4666f16a
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2021-01-06 22:35:31 -0500

    111. Minimum Depth of Binary Tree

---
+++ Problem0111.java
@@ -0,0 +1,41 @@
+/* 111. Minimum Depth of Binary Tree
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/
+ */
+package leetCode;
+
+public class Problem0111 {
+	public static void main(String[] args) {
+		
+		TreeNode t1 = new TreeNode(-9);
+		TreeNode t2 = new TreeNode(-3);
+		TreeNode t3 = new TreeNode(2);
+		TreeNode t4 = new TreeNode(4);
+		TreeNode t5 = new TreeNode(4);
+		TreeNode t6 = new TreeNode(0);
+		TreeNode t7 = new TreeNode(-6);
+		TreeNode t8 = new TreeNode(-5);
+		
+		t1.left = t2;
+		t1.right = t3;
+		t2.right = t4;
+		t3.left = t5;	
+		t3.right = t6;	
+		t4.left = t7;
+		t5.left = t8;
+
+		System.out.println(minDepth(t1));
+		
+	}
+	
+    public static int minDepth(TreeNode root) {
+        if(root == null) {return 0;}
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if(left == 0 || right == 0) {
+        	return Math.max(left, right) + 1;
+        }
+        return Math.min(left, right) + 1;
+    }
+	
+    
+}
